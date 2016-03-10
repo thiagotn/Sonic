38e4ea9e941845e4df21cc810755299aae5aead5
Usuário Convidado <Guest@l2304micro29.local>
2016-03-10 19:48:16 -0300

    Initial Commit

---
+++ Sonic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sonic
+//
+//  Created by Usuário Convidado on 10/03/16.
+//  Copyright © 2016 Thiago Nogueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
