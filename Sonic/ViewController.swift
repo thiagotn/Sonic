b5c3bd6e1b5ee8ec81a8ff152d9dcfcb6bb8f9d1
Usuário Convidado <Guest@L2304MICRO29.local>
2016-03-10 21:07:12 -0300

    Primeiro commit

--- Sonic/ViewController.swift
+++ Sonic/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgSonic: UIImageView!
+
+    var imgs = [
+        UIImage(contentsOfFile: ""),
+        UIImage(contentsOfFile: ""),
+        UIImage(contentsOfFile: ""),
+        UIImage(contentsOfFile: ""),
+        UIImage(contentsOfFile: ""),
+        UIImage(contentsOfFile: ""),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func saltar(sender: UIButton) {
+        print("Botao saltar acionado")
+        var actualPoint:CGPoint = self.imgSonic.center
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            actualPoint = self.imgSonic.center
+            self.imgSonic.center.y = -10 //CGPointMake(180, 200)
+        })
+        self.imgSonic.center = actualPoint
+    }
 
+    @IBAction func girar(sender: AnyObject) {
+        print("Botao girar acionado")
+        var actualPoint:CGPoint = self.imgSonic.center
+        let angle:CGFloat = (180.0 * CGFloat(M_PI) / 180.0)
+        UIView.animateWithDuration(0.3, animations: {() -> Void in
+            actualPoint = self.imgSonic.center
+            self.imgSonic.transform = CGAffineTransformMakeRotation(angle)
+        })
+        self.imgSonic.center = actualPoint
+    }
+
+    @IBAction func correr(sender: UIButton) {
+        print("Botão correr acionado")
+    
+    }
 }
 
